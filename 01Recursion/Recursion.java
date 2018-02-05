613697c3da3fd12c2d39176be3773e45255667f8
Andy Zhou <azhou4@stuy.edu>
2018-02-04 23:01:33 -0500

    added exception for fib

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,5 +1,13 @@
 public class Recursion{
     public static void main(String[] args){
+	//System.out.println(fact(4));
+	//System.out.println(fact(5));
+	//System.out.println(fact(-5));
+	//System.out.println(fib(4));
+	//System.out.println(fib(5));
+	//System.out.println(fib(6));
+	//System.out.println(fib(7));
+	//System.out.println(fib(-8));
     }
     public static int fact(int n){
 	if(n < 0){
@@ -11,6 +19,9 @@ public class Recursion{
 	return n * fact(n-1);
     }
     public static int fib(int n){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
 	if(n == 0){
 	    return 0;
 	}
