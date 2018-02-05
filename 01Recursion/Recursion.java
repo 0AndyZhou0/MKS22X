338892e549b05677e05713329dff8a132369e4c7
Andy Zhou <azhou4@stuy.edu>
2018-02-04 22:56:39 -0500

    added throw exception for factorial

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,8 +1,12 @@
 public class Recursion{
     public static void main(String[] args){
+	
     }
     public static int fact(int n){
-	if (n < 1){
+	if(n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if(n == 0){
 	    return 1;
 	}
 	return n * fact(n-1);
