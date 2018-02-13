f05e0ded1143e6d17ae16ae136c6332190e207ff
Andy Zhou <azhou4@stuy.edu>
2018-02-13 18:41:10 -0500

    added moves in solveH

--- 03KnightlyKnights/KnightBoard.java
+++ 03KnightlyKnights/KnightBoard.java
@@ -1,6 +1,7 @@
 public class KnightBoard{
     public static void main(String[] args){
-	System.out.println("arrrrrrgggggggggggghhh");
+        KnightBoard a = new KnightBoard(8,8);
+	a.solve();
     }
     private int[][] board;
 
@@ -33,6 +34,20 @@ public class KnightBoard{
     }
 
     private boolean solveH(int row ,int col, int level){
+	if(level == board.length * board[0].length){
+	    return true;
+	}
+	int[] xMoves = {-2,-2,-1,1,2,2,1,-1};
+	int[] yMoves = {1,-1,-2,-2,-1,1,2,2};
+	board[row][col] = level;
+	for(int i = 0;i<8;i++){
+	    int nextX = row + xMoves[i];
+	    int nextY = col + yMoves[i];
+	    if(nextX >= 0 && nextY >= 0 && nextX < board.length && nextY < board[0].length){
+		solveH(nextX,nextY,level+1);
+	    }
+	}
+	board[row][col] = 0;
 	return true;
     }
     // level is the # of the knight
