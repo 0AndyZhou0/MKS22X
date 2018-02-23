7598a7b22d489f20309d0d4ac7dad7823ca0c809
Andy Zhou <azhou4@stuy.edu>
2018-02-23 15:33:49 -0500

    added exceptions

--- 03KnightlyKnights/KnightBoard.java
+++ 03KnightlyKnights/KnightBoard.java
@@ -1,18 +1,22 @@
 public class KnightBoard{
     public static void main(String[] args){
         KnightBoard a = new KnightBoard(5,5);
-	//System.out.println(a.solve());
-	//System.out.println(a);
+	System.out.println(a);
+	System.out.println(a.solve(0,0));
+	System.out.println(a);
 	//System.out.println(a.checkSol(0,0,1));
 	//a.board[0][0] = 5;
 	//System.out.println(a.checkSol(0,0,1));
 	//System.out.println(a);
-	System.out.println(a.countSolutions());
+	//System.out.println(a.countSolutions(0,0));
     }
     private int[][] board;
     private int[] moves = {-2,-2,-1,1,2,2,1,-1,1,-1,-2,-2,-1,1,2,2};
     private int solutions;
 
+    /*
+      @throws IllegalArgumentException when either parameter is negative.
+     */
     public KnightBoard(int startingRows,int startingCols){
 	if(startingRows <= 0 || startingCols <= 0){
 	    throw new IllegalStateException();
@@ -36,39 +40,18 @@ public class KnightBoard{
 	return grid;
     }
 
-    public boolean solve(){
-        return solveH(0,0,1);
-    }
-
-    public int countSolutions(){
-        solutions = 0;
-	countSolutionsH(0,0,0,solutions);
-	return solutions;
-    }
-
-    public boolean countSolutionsH(int row ,int col, int level, int solutions){
-        if(level == board.length*board[0].length + 1){
-	    solutions+=1;
-	    return false;
+    /*
+      @throws IllegalStateException when the board contains non-zero values.
+      @throws IllegalArgumentException when either parameter is negative 
+      or out of bounds.
+     */
+    public boolean solve(int startingRow,int startingCol){
+        if(checkZeros() && startingRow < 0 || startingCol < 0){
+	    throw new IllegalArgumentException();
 	}
-	if(board[row][col] != 0){
-	    return false;
-	}
-	int[] order = heuristic(row,col);
-	board[row][col] = level;
-	for(int i = 0;i<8;i++){
-	    int nextR = row + moves[order[i]];
-	    int nextC = col + moves[order[i]+8];
-	    if(nextR >= 0 && nextC >= 0 && nextR < board.length && nextC < board[0].length){
-	    	if(solveH(nextR,nextC,level+1)){
-	    	    return true;	
-	    	}
-	    }
-	}
-	board[row][col] = 0;
-	return false;
+        return solveH(startingRow,startingCol,1);
     }
-
+    
     private boolean solveH(int row ,int col, int level){
 	if(level == board.length*board[0].length + 1){
 	    return true;
@@ -90,8 +73,8 @@ public class KnightBoard{
 	board[row][col] = 0;
 	return false;
     }
-    // level is the # of the knight
 
+    
     private int[] heuristic(int row,int col){
 	int[] moveGoodness = {9,9,9,9,9,9,9,9};
 	for(int i = 0;i < 8;i++){
@@ -118,6 +101,7 @@ public class KnightBoard{
 	return numMoves;
     }
 
+    //Sorts the order of moves
     private int[] SortMin(int[] moveGoodness){
 	int[] order = new int[8];
 	for(int i = 0;i < 8;i++){
@@ -150,5 +134,53 @@ public class KnightBoard{
 	    }
 	}
 	return false;
+    }
+
+    /*
+      @throws IllegalStateException when the board contains non-zero values. 
+      @throws IllegalArgumentException when either parameter is negative 
+      or out of bounds.
+    */
+    public int countSolutions(int startingRow,int startingCol){
+        if(checkZeros() && startingRow < 0 || startingCol < 0){
+	    throw new IllegalArgumentException();
+	}
+        solutions = 0;
+	countSolutionsH(startingRow,startingCol,0,solutions);
+	return solutions;
+    }
+
+    public boolean countSolutionsH(int row ,int col, int level, int solutions){
+        if(level == board.length*board[0].length + 1){
+	    solutions+=1;
+	    return false;
+	}
+	if(board[row][col] != 0){
+	    return false;
+	}
+	int[] order = heuristic(row,col);
+	board[row][col] = level;
+	for(int i = 0;i<8;i++){
+	    int nextR = row + moves[order[i]];
+	    int nextC = col + moves[order[i]+8];
+	    if(nextR >= 0 && nextC >= 0 && nextR < board.length && nextC < board[0].length){
+	    	if(solveH(nextR,nextC,level+1)){
+	    	    return true;	
+	    	}
+	    }
+	}
+	board[row][col] = 0;
+	return false;
+    }
+    
+    private boolean checkZeros(){
+	for(int x = 0;x < board.length;x++){
+	    for(int y = 0;y < board[0].length;y++){
+		if(board[x][y] != 0){
+		    throw new IllegalStateException();
+		}
+	    }
+	}
+	return true;
     }
 }
