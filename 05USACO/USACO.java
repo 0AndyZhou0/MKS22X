18d2de04e2762f6fc5d8ea3b5abe4950d1365cf7
Andy Zhou <azhou4@stuy.edu>
2018-03-07 14:45:17 -0500

    read Bronze problem

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,4 +1,14 @@
+import java.util.*;
+import java.io.*;
 public class USACO{
-    public static int bronze(String filename);
+    public static int bronze(String filename) throws FileNotFoundException{
+	try{
+	    File f = new File(filename);
+	    Scanner s = new Scanner(f);
+	    
+	}catch(FileNotFoundException e){
+	    System.exit(1);
+	}
+    }
     public static int silver(String filename);
 }
