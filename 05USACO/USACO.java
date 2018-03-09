23fb1e3a9d147c71196f0656eecb1e20fd76e4df
Andy Zhou <azhou4@stuy.edu>
2018-03-09 14:45:19 -0500

    finished parsing silver

--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -1,7 +1,17 @@
 import java.util.*;
 import java.io.*;
 public class USACO{
-    public static int bronze(String filename) throws FileNotFoundException{
+    
+    public static void main(String[] args){
+	System.out.println(USACO.bronze("makelake/makelake.1.in"));
+	System.out.println(USACO.silver("ctravel/ctravel.1.in"));
+    }
+    
+	    
+    public static int bronze(String filename){
+	int row, col, elevation;
+	int numInstructions;
+	
 	try{
 	    File f = new File(filename);
 	    Scanner s = new Scanner(f);
@@ -9,29 +19,39 @@ public class USACO{
 	}catch(FileNotFoundException e){
 	    System.exit(1);
 	}
+	return 0;
     }
-    public static int silver(String filename) throws FileNotFoundException{
+    public static int silver(String filename){
 	char[][] grid;
 	int start1, end1, start2, end2, time;
 	int[][] numWays;//current number of ways to get to each space.
-	int[][] temp;
+	int[][] tempGrid;
 	try{
 	    File f = new File(filename);
 	    Scanner s = new Scanner(f);
-	    int i = 0;
 	    String temp = s.nextLine();
 	    String[] nums = temp.split(" ");
-	    grid = new char[Integer.parseInt(nums[0])][Integer.parseInt(nums[0])];
-	    time = Integer.parseInt(nums[3]);
+	    grid = new char[Integer.parseInt(nums[0])][Integer.parseInt(nums[1])];
+	    time = Integer.parseInt(nums[2]);
+	    int i = 0;
 	    while(s.hasNextLine()){
-		String temp = s.nextLine();
-		char[] line = new char[temp.length()];
-		for(int x = 0;x < line.length;x++){
-		    
+		temp = s.nextLine();
+		if(s.hasNextLine()){
+		    for(int x = 0;x < temp.length();x++){
+			grid[i][x] = temp.charAt(x);
+		    }
+		}else{
+		    String[] line = temp.split(" ");
+		    start1 = Integer.parseInt(line[0]);
+		    end1 = Integer.parseInt(line[1]);
+		    start2 = Integer.parseInt(line[2]);
+		    end2 = Integer.parseInt(line[3]);
 		}
 		i++;
 	    }
 	}catch(FileNotFoundException e){
 	    System.exit(1);
 	}
+	return 0;
+    }
 }
