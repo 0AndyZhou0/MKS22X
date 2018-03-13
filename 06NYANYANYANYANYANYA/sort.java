a2eed75c359a71104e6e8b129997fc3473c4ab23
Andy Zhou <azhou4@stuy.edu>
2018-03-13 14:24:00 -0400

    tested partition

--- 06NYANYANYANYANYANYA/sort.java
+++ 06NYANYANYANYANYANYA/sort.java
@@ -1,26 +1,49 @@
 public class sort{
-	public static void main(String[] args){
+    public static void main(String[] args){
+	int[] ary = {4,1,3,5,3,2,5};
+	print(ary);
+	partition(ary);
+        print(ary);
+    }
 
-	}
-
-/*
-	int[] list;
-	public sort(int[] nums){
-		list = new int[nums.length];
-		for(int i = 0;i < nums.length;i++){
-			list[i] = nums[i];
-		}
-	}
-*/
+    /*
+      int[] list;
+      public sort(int[] nums){
+      list = new int[nums.length];
+      for(int i = 0;i < nums.length;i++){
+      list[i] = nums[i];
+      }
+      }
+    */
 	
-	public int[] partition(int[] nums){
-		int partition = nums[0];
-		for(int i = 1;i < nums.length;i++){
-			if(nums[i]
+    public static void partition(int[] nums){
+	int partition = nums[0];
+	int x = nums.length - 1;
+	for(int i = 1;i < x;i++){
+	    if(nums[i] > partition){
+		while(nums[x] > partition && x > i){
+		    x--;
 		}
+		swap(nums,i,x);
+	    }
 	}
+	for(int i = 1;i < x;i++){
+	    swap(nums,i,i-1);
+	}
+	nums[x - 1] = partition;
+    }
+    
+    public static void swap(int[]nums, int num1, int num2){
+	int temp = nums[num1];
+	nums[num1] = nums[num2];
+	nums[num2] = temp;
+    }
 
-	public int[] swap(int[]nums, int num1, int num2){
-		nums
+    public static void print(int[] ary){
+	String line = "[";
+	for(int i = 0;i < ary.length;i++){
+	    line += ary[i] + ",";
 	}
+        System.out.println(line + "]");
+    }
 }
