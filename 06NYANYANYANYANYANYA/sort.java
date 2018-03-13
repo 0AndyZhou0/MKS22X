21c5a1dc295301f6eec9c5b179e7422a512a232a
azhou4 <azhou4@STUYSDC.LOCAL>
2018-03-13 10:13:54 -0400

    added fie

---
+++ 06NYANYANYANYANYANYA/sort.java
@@ -0,0 +1,26 @@
+public class sort{
+	public static void main(String[] args){
+
+	}
+
+/*
+	int[] list;
+	public sort(int[] nums){
+		list = new int[nums.length];
+		for(int i = 0;i < nums.length;i++){
+			list[i] = nums[i];
+		}
+	}
+*/
+	
+	public int[] partition(int[] nums){
+		int partition = nums[0];
+		for(int i = 1;i < nums.length;i++){
+			if(nums[i]
+		}
+	}
+
+	public int[] swap(int[]nums, int num1, int num2){
+		nums
+	}
+}
