7ac2d250833fab3e6e57582997565662fc33afa9
Andy Zhou <azhou4@stuy.edu>
2018-03-15 23:34:37 -0400

    fixed partition to work with an array set

--- 06NYANYANYANYANYANYA/quick.java
+++ 06NYANYANYANYANYANYA/quick.java
@@ -2,10 +2,12 @@ public class quick{
     public static void main(String[] args){
 	//int[] ary = {999,999,999,4,1,0,3,2,999,999,999};
 	//int[] ary = {0,999,999,999,1999,0,3,2,999,999,999};
-	int[] ary = {0,1,2,3,4,5,6,1,2,4,5};
+	//int[] ary = {0,1,2,3,4,5,6,1,2,4,5};
 	//int[] ary = {0,0,0,0,0,1,1,1,1,1};
+	int[] ary = {0,0,0,0,0,0,0,0,0,0,0,0,0};
 	print(ary);
-	System.out.println(partition(ary,0,ary.length-1));
+	//System.out.println(partition(ary,0,ary.length-1));
+	System.out.println(quickselect(ary,5));
         print(ary);
     }
 
@@ -26,29 +28,30 @@ public class quick{
 	System.out.println(index);
 	swap(nums,index,min);
 	int partition = nums[min];
-	//int copies = 1;
-	min++;
-	while(min < max){
-	    if(nums[min] > partition){
-		while(nums[max] >= partition && max > min){
+	int copies = 1;
+	for(int i = min + 1;i < max;i++){
+	    print(nums);
+	    if(nums[i] >= partition){
+		while(nums[max] > partition && max > i){
 		    print(nums);
 		    max--;
 		}
-		swap(nums,min,max);
+		swap(nums,i,max);
 	    }
-	    // if(nums[min] == partition){
-	    // 	swap(nums,copies,min);
-	    // 	copies++;
-	    // }
-	    min++;
+	     if(nums[i] == partition){
+	     	swap(nums,copies,i);
+	     	copies++;
+	     }
 	}
-	print(nums);
-	if(nums[min] > nums[max-1]){
-	    swap(nums,min,max-1);
+	for(int i = min + 1;i < max;i++){
+	    swap(nums,i,max);
+	}
+	if(nums[min] >= nums[max]){
+	    swap(nums,min,max);
 	}else{
-	    swap(nums,min,max-2);
+	    swap(nums,min,max-1);
 	}
-	return max - 1;
+	return max;
     }
     
     public static void swap(int[]nums, int num1, int num2){
@@ -76,7 +79,11 @@ public class quick{
 	}
 	int index2 = 0;
 	if(index < k - 1){
+	    index2 = partition(nums,index+1,nums.length-1);
+	    return findKH(nums,k,index2);
+	}else{
+	    index2 = partition(nums,0,index-1);
+	    return findKH(nums,k,index2);
 	}
-	return findKH(nums,k,index2);
     }
 }
