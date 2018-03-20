d406a4536f4a414d7e2927e561df4e2ba8523e18
Andy Zhou <azhou4@stuy.edu>
2018-03-20 00:07:19 -0400

    merged files

--- 06NYANYANYANYANYANYA/quick.java
+++ 06NYANYANYANYANYANYA/quick.java
@@ -10,8 +10,19 @@ public class Quick{
 	//System.out.println(partition(ary,0,ary.length-1));
 	//System.out.println(quickselect(ary,5));
 	quickSort(ary);
-	print(ary);
+        print(ary);
     }
+
+    /*
+      int[] list;
+      public sort(int[] nums){
+      list = new int[nums.length];
+      for(int i = 0;i < nums.length;i++){
+      list[i] = nums[i];
+      }
+      }
+    */
+
     
     public static int partition(int[] nums, int min, int max){
 	int index = (int)(Math.random() * (max - min + 1)) + min;
@@ -35,6 +46,23 @@ public class Quick{
 	swap(nums,j,min);
 	return j;
     }
+    
+
+    // public static int partition(int[] nums, int min, int max){
+    //     swap(nums,(int)(Math.random() * (max - min + 1)) + min,min);
+    // 	int partition = nums[min];
+    // 	int i = min;
+    // 	int j = min + 1;
+    // 	while(j < max + 1){
+    // 	    if(nums[j] <= partition){
+    // 		i++;
+    // 		swap(nums,i,j);
+    // 	    }
+    // 	    j++;
+    //     }
+    // 	swap(nums,i,min);
+    // 	return i;
+    // }
     
     public static void swap(int[]nums, int num1, int num2){
 	int temp = nums[num1];
@@ -70,7 +98,9 @@ public class Quick{
     }
 
     public static void quickSort(int[]nums){
-        quickSortH(nums,0,nums.length-1);
+	int pivot = partition(nums,0,nums.length-1);
+	quickSortH(nums,pivot+1,nums.length-1);
+	quickSortH(nums,0,pivot-1);
     }
     
     public static void quickSortH(int[]nums,int min,int max){
