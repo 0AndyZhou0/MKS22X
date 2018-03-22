34fc557ab6bcb23fc8d51f678637ab65cad73fd2
Andy Zhou <azhou4@stuy.edu>
2018-03-21 22:48:22 -0400

    fixed and tested, it works

--- 07EMERGEingNonExistentTalent/Merge.java
+++ 07EMERGEingNonExistentTalent/Merge.java
@@ -1,55 +1,64 @@
 public class Merge{
     public static void main(String[] args){
-	int[] ary = {1,2,54,1,56,4,2,2,6,34,23,6,2};
+	//int[] ary = {1,2,54,1,56,4,2,2,6,34,23,6,2};
+	//int[] ary = {0,0,0,0,0,0,0,0,0};
+	int[] ary = {1,1,1,1,4,4,4,4,4,1,1,1,1};
+	System.out.println(print(ary));
 	mergesort(ary);
-	print(ary);
+	System.out.println(print(ary));
     }
 
     public static void mergesort(int[]data){
 	int[] temp = new int[data.length];
+	for(int i = 0;i < data.length;i++){
+	    temp[i] = data[i];
+	}
 	msort(data,temp,0,data.length-1);
     }
 	
     private static void msort(int[]data, int[]temp, int lo, int hi){
 	if(hi > lo){
-	    msort(temp,data,lo,(hi-lo+1)/2);
-	    msort(temp,data,(hi-lo+3)/2,hi);
+	    for(int i = lo;i <= hi;i++){
+		temp[i] = data[i];
+	    }
+	    int split = (hi+lo)/2;
+	    msort(temp,data,lo,split);
+	    msort(temp,data,split+1,hi);
 	    merge(data,temp,lo,hi);
 	}
     }
 
     private static void merge(int[]data,int[]temp,int lo,int hi){
-	int split1 = (hi-lo+1)/2;
-	int i = 0;
-	int j = 0;
+	int split = (hi+lo)/2;
+	int i = lo;
+	int j = split + 1;
 	int index = lo;
-	//while(i <= split1 && j <= hi){
-	
-	    if(data[i] <= data[j]){
-		temp[index] = data[i];
+	while(i <= split && j <= hi){
+	    if(temp[i] <= temp[j]){
+		data[index] = temp[i];
 		i++;
 	    }else{
-		temp[index] = data[j];
+		data[index] = temp[j];
 		j++;
 	    }
 	    index++;
 	}
-	// while(i <= split1){
-	//     temp[index] = data[i];
-	//     i++;
-	//     index++;
-	// }
-	// while(j <= hi){
-	//     temp[index] = data[j];
-	//     j++;
-	//     index++;
-	// }
+	while(i <= split){
+	    data[index] = temp[i];
+	    i++;
+	    index++;
+	}
+	while(j <= hi){
+	    data[index] = temp[j];
+	    j++;
+	    index++;
+	}
     }
 
     public static String print(int[] ary){
 	String line = "[";
 	    for(int i = 0;i < ary.length-1;i++){
-		line+= i + ",";
+		line+= ary[i] + ",";
 	    }
 	line += ary[ary.length-1] + "]";
 	return line;
