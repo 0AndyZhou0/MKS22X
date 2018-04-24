8973b41057aae95a133d537358d0c4071b521430
Andy Zhou <azhou4@stuy.edu>
2018-04-24 14:45:42 -0400

    finished classwork and fixed most of Deque

---
+++ 09PostFixed/#Calulator.java#
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Calculator{
+    public static double eval(String s){
+	String[] temp = s.split(" ");
+	//LinkedList<String> list = new LinkedList<>();
+	while(){
+	    for(int i = 0;i < ary.length;i++){
+		String s = temp[i];
+		if(s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("%")){
+		    
+		}
+	    }
+	}
+    }
+}
--- 10DEQOfCards/MyDeque.java
+++ 10DEQOfCards/MyDeque.java
@@ -2,40 +2,39 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class MyDeque<E>{
     public static void main(String[] args){
-	MyDeque<String> a = new MyDeque();
-	
-	a.addFirst("hi");
-	System.out.println(a);
-	a.addLast("bye");
-	System.out.println(a);
-	a.addLast("tree");
-	System.out.println(a);
-	a.addFirst("friend");
-	System.out.println(a);
-	// a.addFirst("hi");
-	// System.out.println(a);
-	// a.addLast("bye");
-	// System.out.println(a);
-	// a.addLast("tree");
-	// System.out.println(a);
-	// a.addFirst("friend");
-	// System.out.println(a);
-	// a.addFirst("hi");
-	// System.out.println(a);
-	// a.addLast("bye");
-	// System.out.println(a);
-	// a.addLast("tree");
-	// System.out.println(a);
-	// a.addFirst("friend");
-	// System.out.println(a);
-	// a.addFirst("hi");
-	// System.out.println(a);
-	// a.addLast("bye");
-	// System.out.println(a);
-	// a.addLast("tree");
-	// System.out.println(a);
-	// a.addFirst("friend");
-	// System.out.println(a);
+	MyDeque<String> a = new MyDeque<>(), a1 = new MyDeque<>();
+	ArrayList<String> b = new ArrayList<>();
+
+	int size = Integer.parseInt(args[0]);
+	for(int i = 0; i < size; i++){
+	    int temp = (int)(Math.random() * 1000);
+	    if(temp % 2 == 0){
+		a.addFirst("" + temp);
+		a1.addFirst("" + temp);
+		b.add(0, "" + temp);
+	    }
+	    else{
+		a.addLast("" + temp);
+		a1.addLast("" + temp);
+		b.add("" + temp);
+	    }
+	}
+
+	int index = 0;
+	boolean hasError = false;
+	String errorEvaluation = "Errors found at these indices: ";
+	for (String x : b){
+	    if (!(x.equals(a.getFirst()))){
+		System.out.println("The getFirst() function is incorrect at index " + index);
+		hasError = true;
+	    }
+	    if (!(x.equals(a.removeFirst()))){
+		System.out.println("There is an error at index " + index);
+		errorEvaluation += index + ", ";
+		hasError = true;
+	    }
+	    index++;
+	}
     }
     
     private E[] ary;
@@ -59,7 +58,7 @@ public class MyDeque<E>{
 
     public String toString(){
 	String line = "[";
-	for(int i = first;i <= last;i++){
+	for(int i = first;i != last;i++){
 	    if(i == ary.length){
 		i = 0;
 	    }
@@ -154,7 +153,7 @@ public class MyDeque<E>{
     private void resize(){
 	E[] bigger = (E[]) new Object[size*2];
 	int index = 0;
-	for(int i = first;i <= last;i++){
+	for(int i = first;i != last;i++){
 	    if(i == ary.length){
 		i = 0;
 	    }
