18c38b77f3dbf9f4b85af0756061eb764bfae930
Andy Zhou <azhou4@stuy.edu>
2018-04-27 14:23:30 -0400

    added resize

--- 13HEAPsOfSHEAPle/MyHeap.java
+++ 13HEAPsOfSHEAPle/MyHeap.java
@@ -1,3 +1,47 @@
 public class MyHeap{
+    private String[] ary;
+    private int size = 0;
+    private boolean MaxMin = true;
+    
+    public MyHeap(){
+	ary = new String[15];
+    }
 
+    public MyHeap(boolean){
+	ary = new String[15];
+	MaxMin = boolean;
+    }
+
+    public void add(String s){
+	if(size == ary.length - 1){
+	    resize();
+	}
+	
+    }
+
+    public String remove(){
+
+    }
+
+    public String peek(){
+
+    }
+
+    public int size(){
+	return size;
+    }
+
+    public void resize(){
+	String[] bigger = new String[ary.length * 2];
+	for(int i = 0;i < ary.length;i++){
+	    bigger[i] = ary[i];
+	}
+	ary = bigger;
+    }
+    
+    public void swap(String[] ary,int a,int b){
+	String temp = ary[a];
+	ary[a] = ary[b];
+	ary[b] = temp;
+    }
 }
