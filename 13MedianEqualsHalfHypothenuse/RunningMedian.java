22e45d94a9b44caeeb15b0d53684db14f55f8327
Andy Zhou <azhou4@stuy.edu>
2018-05-07 14:00:38 -0400

    started 13 and 14

---
+++ 13MedianEqualsHalfHypothenuse/RunningMedian.java
@@ -0,0 +1,16 @@
+public class RunningMedian{
+    private MyHeap<Double> smaller,larger;
+    public RunningMedian(){
+	
+    }
+
+    public void add(Double){
+	
+    }
+
+    public Double getMedian(){
+	return median;
+    }
+
+    public in size =
+}
